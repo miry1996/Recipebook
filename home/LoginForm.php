fb2480fe40e04341232f016c5b40c6798003ace1
miry1996 <43436228+miry1996@users.noreply.github.com>
2018-11-14 08:09:58 +0100

    Update LoginForm.php

--- home/LoginForm.php
+++ home/LoginForm.php
@@ -9,13 +9,13 @@
     <form action="LoginConnection.php" method="GET">
        
 			Enter your user name:<br>
-			<input type="password" name="username">
+			<input type="text" name="username">
 			<br>
 			<br>
 	  
 
             Enter your password:<br>
-            <input type="text" name="pwd">
+            <input type="password" name="pwd">
             <br>
 			<br>
 			<input type="submit" value="Send input!">
